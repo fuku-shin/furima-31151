8cae4fa91cf26d9fb6f779411bc307dcb4603acd
fuku-shin <bmtubashinpi@yahoo.co.jp>
2020-11-10 18:30:48 +0900

    不要な記述の削除とrubocopでの修正

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -3,7 +3,6 @@ window.addEventListener('load', function(){
   const countPrice  = document.getElementById("item-price");
   countPrice.addEventListener("keyup", () => {
     const itemPrice = countPrice.value;
-    console.log(itemPrice)
     const taxPrice  = document.getElementById("add-tax-price");
     const tax = (itemPrice * 0.1) ;
     taxPrice.innerHTML = tax;
