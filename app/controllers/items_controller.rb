a99baac2fe64f96a41f5be5bf585470ad0e81385
fuku-shin <bmtubashinpi@yahoo.co.jp>
2020-11-12 16:49:22 +0900

    rubocoによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,10 +22,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-      @item = Item.find(params[:id])
-      if current_user.id != @item.user.id
-        redirect_to root_path
-      end
+    @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user.id
   end
 
   def update
